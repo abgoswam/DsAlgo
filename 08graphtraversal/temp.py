4ba6768926e43e0897868daa21508f0cba7ae1fb
Abhishek Goswami <abgoswam@gmail.com>
2017-01-03 09:59:11 -0800

    adding readme

---
+++ 08graphtraversal/temp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 27 10:11:34 2016
+
+@author: abgoswam
+"""
+
+import sys
+
+if __name__ == "__main__":
+    print("abhishek")
+    sys.exit()
+    print("goswami")
+
---
+++ README.md
@@ -0,0 +1,13 @@
+1. 01sumswap
+  - hashmap
+2. 02mergesort
+  - divide and conquer
+3.
+4.
+5.
+6.
+7.
+8. 08graphtraversal
+  - stack, queue
+9. 09queue
+  - stack, queue
